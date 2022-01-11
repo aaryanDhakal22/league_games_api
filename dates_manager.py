75369d34800b94161e78eaa65c0faddccbbd9b8d
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-01-11 19:41:07 +0545

    FInally prototype works as intended
    Bug was api not accepting events of 30 sec difference and also calender_manager proved to be useless so all imports shifted to main.py

--- dates_manager.py
+++ dates_manager.py
@@ -3,58 +3,65 @@ from datetime import datetime
 import requests
 from pprint import pprint
 
+
 class Lolesports_API:
-    API_KEY = {'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'}
-    API_URL = 'https://esports-api.lolesports.com/persisted/gw'
-    
+    API_KEY = {"x-api-key": "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z"}
+    API_URL = "https://esports-api.lolesports.com/persisted/gw"
+
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update(self.API_KEY)
 
-    def get_leagues(self, hl='en-US'):
-        response =  self.session.get(
-            self.API_URL + '/getLeagues',
-            params={'hl': hl}
-        )
-        return json.loads(response.text)['data']
+    def get_leagues(self, hl="en-US"):
+        response = self.session.get(self.API_URL + "/getLeagues", params={"hl": hl})
+        return json.loads(response.text)["data"]
 
-    def get_schedule(self, hl='en-US', league_id=None, pageToken=None):
+    def get_schedule(self, hl="en-US", league_id=None, pageToken=None):
         response = self.session.get(
-            self.API_URL + '/getSchedule',
-            params={
-                'hl': hl,
-                'leagueId': league_id,
-                'pageToken': pageToken
-            }
+            self.API_URL + "/getSchedule",
+            params={"hl": hl, "leagueId": league_id, "pageToken": pageToken},
         )
 
-        return json.loads(response.text)['data']
+        return json.loads(response.text)["data"]
 
 
 def schedule_return():
     api = Lolesports_API()
     lck_id = "98767991310872058"
     lpl_id = "98767991314006698"
-    schedule_now = api.get_schedule( league_id =[ int(lck_id)])['schedule']
-    newer_page_id = schedule_now['pages']['newer']
-    schedule_newer = api.get_schedule(league_id=[int(lck_id)], pageToken=newer_page_id)['schedule']['events']
-    all_schedule = schedule_now['events'] + schedule_newer
+    schedule_now = api.get_schedule(league_id=[int(lck_id)])["schedule"]
+    newer_page_id = schedule_now["pages"]["newer"]
+    schedule_newer = api.get_schedule(league_id=[int(lck_id)], pageToken=newer_page_id)[
+        "schedule"
+    ]["events"]
+    all_schedule = schedule_now["events"] + schedule_newer
 
     final_games_total = []
     for i in all_schedule:
-        if int(i['startTime'][:4]) >= 2022:
-            team = i['match']['teams']
-            year,month,day = i['startTime'][:10].split('-')
-            hour,min = i['startTime'][11:16].split(":")
-            checker = i['startTime'].replace("T","-")[:-1]
+        if int(i["startTime"][:4]) >= 2022:
+            team = i["match"]["teams"]
+            year, month, day = i["startTime"][:10].split("-")
+            hour, minute = i["startTime"][11:16].split(":")
+            checker = i["startTime"].replace("T", "-")[:-1]
             # print(checker)
-            record = [team[0]['name'],team[1]['name'],i['startTime'][:-1],checker]
+            record = [
+                team[0]["name"],
+                team[1]["name"],
+                year,
+                month,
+                day,
+                hour,
+                minute,
+                checker,
+            ]
 
             final_games_total.append(record)
-            result = sorted(final_games_total,key= lambda date: datetime.strptime(date[-1], "%Y-%m-%d-%H:%M:%S"))
+            result = sorted(
+                final_games_total,
+                key=lambda date: datetime.strptime(date[-1], "%Y-%m-%d-%H:%M:%S"),
+            )
             # print(result)
             pass
     # pprint(schedule_newer)
     # pprint(type(schedule_now))
     return result
-
--- main.py
+++ main.py
@@ -1,72 +1,47 @@
-import calender_manager as calm
 import dates_manager as dtm
-from pprint import pprint
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
-matches = dtm.schedule_return()[:8]
-creds = calm.check_creds()
-service = build('calendar', 'v3', credentials=creds)
+SCOPES = [
+    "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/calendar.events.readonly",
+    "https://www.googleapis.com/auth/calendar.events.owned",
+    "https://www.googleapis.com/auth/calendar.events",
+]
 
+creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+service = build("calendar", "v3", credentials=creds)
+
+matches = dtm.schedule_return()
 for match in matches:
+    team1, team2, year, month, day, hour, minute, checker = match
+    event_name = team1 + " vs " + team2
+    date = "-".join([year, month, day]) + "T"
+    start_time = date + ":".join([hour, minute, "00"])
+    timeZone = "Etc/UTC"
+    hour = str(int(hour) + 1).zfill(2) if hour != "24" else "00"
+    end_time = date + ":".join([hour, minute, "00"])
+
+    print(event_name, start_time, end_time)
     event = {
-            'summary': match[0]+' vs '+match[1],
-            'location': None,
-            'description': 'Match at lolesports.com/schedule',
-            'start': {
-                'dateTime': match[-2],
-                'timeZone': 'Asia/Kathmandu',
-            },
-            'end': {
-                'dateTime': match[-2][:-2]+"60",
-                'timeZone': 'Asia/Kathmandu',
-            },
-            'recurrence': [
-                'RRULE:FREQ=DAILY;COUNT=1'
-            ],
-            'attendees': [
-                None
-            ],
-            'reminders': {
-                'useDefault': True,
-            },
-            }
-    try :
-        event = service.events().insert(calendarId='primary', body=event).execute()
-    except HttpError as error:
-        print('An error occurred: %s' % error)
-    print(event)
-    print(match)
+        "summary": event_name,
+        "description": "Match at lolesports.com/schedule",
+        "start": {
+            "dateTime": start_time,
+            "timeZone": timeZone,
+        },
+        "end": {
+            "dateTime": end_time,
+            "timeZone": timeZone,
+        },
+    }
+    event = (
+        service.events()
+        .insert(
+            calendarId="ilv5csjir2espe1t64i5l5f0mc@group.calendar.google.com",
+            body=event,
+        )
+        .execute()
+    )
     break
-
-#======== templates
-event = {
-  'summary': 'Google I/O 2015',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2015-05-28T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles',
-  },
-  'end': {
-    'dateTime': '2015-05-28T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles',
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=1'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'},
-  ],
-  'reminders': {
-    'useDefault': False,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10},
-    ],
-  },
-}
-
-# event = service.events().insert(calendarId='primary', body=event).execute()
-# print 'Event created: %s' % (event.get('htmlLink'))
